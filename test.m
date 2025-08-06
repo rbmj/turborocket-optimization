4721a4c914c6381d99f6ec59afe6752c92e1545d
rbmj <rbmj@verizon.net>
2025-08-06 07:50:58 -0400

    initial

---
+++ test.m
@@ -0,0 +1,33 @@
+% Don't regen the database, but everything else should get cleared
+% Otherwise weird state can persist in the mixture causing bad things
+clearvars -except DB
+
+import combustiontoolbox.databases.NasaDatabase;
+import combustiontoolbox.rocket.*;
+import combustiontoolbox.core.*;
+
+DB = NasaDatabase();
+
+sys = ChemicalSystem(DB);
+solver = RocketSolver('problemType', 'ROCKET_IAC');
+% Define mixture
+% For some reason the toolbox uses bLb for liquid species
+mix = Mixture(sys);
+
+fuel_mass_frac = 50.75;
+fuel_molar_weight = 2.016;
+oxidizer_molar_weight = 32;
+
+set(mix, {'H2'}, 'fuel', fuel_mass_frac / fuel_molar_weight);
+set(mix, {'O2bLb'}, 'oxidizer', (100 - fuel_mass_frac) / oxidizer_molar_weight);
+mix = setTemperatureSpecies(mix, [200.1, 90]); % Same order as they were added
+mix = setPressure(mix, 4967*0.0689476); % Bar
+%Assume that the phase change enthalpy dominates and everything is at 90K
+%setTemperature(mix, 90);
+fprintf('Temperature set to %f\n', mix.T);
+% Solver crashes if we don't specify an area ratio > 1:
+mix.areaRatio = 2;
+
+print(mix);
+
+[inj_mix, chamber_mix, throat_mix, exit_mix] = solver.solve(mix);
